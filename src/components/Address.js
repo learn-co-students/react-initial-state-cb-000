147f6574ec3a32d5b2aa42b19c4a6ad8bb0a9268
Quinn Dizon <qdizon@gmail.com>
2018-10-23 20:35:45 +0000

    Done.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,8 @@
-// Component Code Goes Here
+// Component Code Goes Hereclass Address extends React.Component {
+class Address extends React.Component{
+  render() {
+    return (
+      <div className="address">{this.props.street}, {this.props.city}</div>
+    );
+  }
+}
